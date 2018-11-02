2a388677604c0c2914d5d1e27b4d6a740901a030
olsamat <4x74av@gmail.com>
2018-11-02 18:12:25 +0200

    first commit

--- CountTurbulence/Program.cs
+++ CountTurbulence/Program.cs
@@ -10,6 +10,39 @@ namespace CountTurbulence
     {
         static void Main(string[] args)
         {
+            string s = "7";
+            int[] arr = new int[12];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(3, 10);
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item+"; ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(CountTurbul(arr));
+        }
+        static int CountTurbul(int[] arr)
+        {
+            int count = 0;
+            int flag = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (flag == 0 && arr[i] != arr[i + 1])
+                {
+                    count++;
+                    flag = (arr[i + 1] - arr[i] > 0) ? 1 : -1;
+                }
+                else if ((flag == -1 && (arr[i + 1] - arr[i] >= 0)) || (flag == 1 && (arr[i + 1] - arr[i] <= 0)))
+                {
+                    count++;
+                    if (arr[i] == arr[i + 1]) flag = 0;
+                    else flag = (arr[i + 1] - arr[i] > 0) ? 1 : -1;
+                }
+            }
+            return count;
         }
     }
 }
